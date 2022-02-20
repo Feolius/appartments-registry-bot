3b02b4a56119905d10858ec2b2bc61b1d923a3f7
ivan <ivan.strygin@managinglife.com>
2022-02-20 20:58:59 +0300

    Added aptslist and delme commands.

--- index.js
+++ index.js
@@ -57,9 +57,21 @@ bot.use((ctx, next) => {
 });
 
 const GENERAL_ERROR_MSG = 'Соррян, что-то пошло не так😔';
+const TELEGRAM_MAX_MESSAGE_LENGTH = 4096;
+const BATCH_MESSAGES_SEND_DELAY_MS = 2000;
 
 mysqlDb()
     .then(db => {
+        let batchSendingInProcess = false;
+        // @TODO add commands pool instead
+        bot.use((ctx, next) => {
+            // Shouldn't respond while batch is sending.
+            if (batchSendingInProcess) {
+                return;
+            }
+            return next();
+        })
+
         bot.command('setapt', async (ctx) => {
             let aptNumber;
             if (ctx.state.command.args.length === 1) {
@@ -73,7 +85,7 @@ mysqlDb()
                 return;
             }
             if (ctx.update.message === undefined) {
-                logger.error('Message object is absent in ctx:');
+                logger.error('setapt: message object is absent in ctx:');
                 logger.error(JSON.parse(JSON.stringify(ctx)));
                 ctx.reply(`У меня ощущение, будто меня как-то неправильно используют🤔`);
                 return;
@@ -87,7 +99,7 @@ mysqlDb()
                     username = ctx.update.message.from.username;
                 }
             } else {
-                logger.error('Cannot find sender user. ctx:');
+                logger.error('setapt: cannot find sender user. ctx:');
                 logger.error(JSON.parse(JSON.stringify(ctx)));
                 ctx.reply(`Ой, а кто это у нас такой скрытный здесь?🤡Даже не понять, кто-ты! (ошибка в определении юзера)`);
                 return;
@@ -109,7 +121,7 @@ mysqlDb()
                     });
                 });
             } catch (err) {
-                logger.error('Error on attempt to find existing record:');
+                logger.error('setapt: error on attempt to find existing record:');
                 logger.error(JSON.parse(JSON.stringify(err)));
                 ctx.reply(GENERAL_ERROR_MSG);
                 return;
@@ -127,7 +139,7 @@ mysqlDb()
                         });
                     });
                 } catch (err) {
-                    logger.error('Error on attempt to insert record:');
+                    logger.error('setapt: error on attempt to insert record:');
                     logger.error(JSON.parse(JSON.stringify(err)));
                     ctx.reply(GENERAL_ERROR_MSG);
                     return;
@@ -144,7 +156,7 @@ mysqlDb()
                         })
                     });
                 } catch (err) {
-                    logger.error('Error on attempt to update existing record:');
+                    logger.error('setapt: error on attempt to update existing record:');
                     logger.error(JSON.parse(JSON.stringify(err)));
                     ctx.reply(GENERAL_ERROR_MSG);
                     return;
@@ -152,6 +164,7 @@ mysqlDb()
             }
             ctx.reply(`Понял-принял👍`);
         })
+
         bot.command('aptcontacts', async (ctx) => {
             let aptNumber;
             if (ctx.state.command.args.length === 1) {
@@ -178,33 +191,185 @@ mysqlDb()
                     });
                 });
             } catch (err) {
-                logger.error('Error on attempt to find existing record:');
+                logger.error('aptcontacts: error on attempt to find existing record:');
                 logger.error(JSON.parse(JSON.stringify(err)));
                 ctx.reply(GENERAL_ERROR_MSG);
             }
             if (contacts.length > 0) {
-                ctx.reply(contacts.map((contact) => {
-                    if (contact.username !== null) {
-                        return `[@${contact.username}](tg://user?id=${contact.user_id})`.replace(/_/g, "\\_");
-                    } else {
-                        return `[без юзернейма](tg://user?id=${contact.user_id})`
-                    }
-                }).join(', '), { parse_mode: 'MarkdownV2' });
+                ctx.reply(renderContacts(contacts), { parse_mode: 'MarkdownV2' });
             } else {
                 ctx.reply("Здесь пока никто не живет. Но это не точно.🤓");
             }
         })
+
+        bot.command('aptslist', async (ctx) => {
+            if (ctx.update.message === undefined) {
+                logger.error('aptslist: message object is absent in ctx:');
+                logger.error(JSON.parse(JSON.stringify(ctx)));
+                ctx.reply(`У меня ощущение, будто меня как-то неправильно используют🤔`);
+                return;
+            }
+            let userId;
+            if (ctx.update.message.from !== undefined) {
+                userId = ctx.update.message.from.id;
+            } else {
+                logger.error('aptslist: cannot find sender user. ctx:');
+                logger.error(JSON.parse(JSON.stringify(ctx)));
+                ctx.reply(`Ой, а кто это у нас такой скрытный здесь?🤡Даже не понять, кто-ты! (ошибка в определении юзера)`);
+                return;
+            }
+
+            const chatId = ctx.update.message.chat.id;
+            let contacts = [];
+            try {
+                contacts = await new Promise((resolve, reject) => {
+                    db.query(`SELECT * FROM apartment_info WHERE chat_id = ? ORDER BY apartment_number`, [chatId], (error, results) => {
+                        if (error) {
+                            reject(error);
+                            return;
+                        }
+                        resolve(results);
+                    });
+                });
+            } catch (err) {
+                logger.error('aptslist: error on attempt to find existing record:');
+                logger.error(JSON.parse(JSON.stringify(err)));
+                ctx.reply(GENERAL_ERROR_MSG);
+                return;
+            }
+
+            const msgs = buildAptListMessages(contacts);
+            let sendMessagesChain = Promise.resolve();
+            batchSendingInProcess = true;
+            for (let msg of msgs) {
+                sendMessagesChain = sendMessagesChain.then(() => {
+                    ctx.telegram.sendMessage(ctx.update.message.from.id, msg, {parse_mode: 'MarkdownV2'});
+                    return new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve(true);
+                        }, BATCH_MESSAGES_SEND_DELAY_MS);
+                    })
+                })
+            }
+            sendMessagesChain
+                .catch(err => {
+                    logger.error('aptslist: error on attempt to send messages:');
+                    logger.error(JSON.parse(JSON.stringify(err)));
+                    setTimeout(() => {
+                        ctx.telegram.sendMessage(ctx.update.message.from.id, GENERAL_ERROR_MSG);
+                        }, BATCH_MESSAGES_SEND_DELAY_MS
+                    );
+
+                })
+                .finally(() => batchSendingInProcess = false);
+        });
+
+        bot.command('delme', async (ctx) => {
+            if (ctx.update.message === undefined) {
+                logger.error('delme: message object is absent in ctx:');
+                logger.error(JSON.parse(JSON.stringify(ctx)));
+                ctx.reply(`У меня ощущение, будто меня как-то неправильно используют🤔`);
+                return;
+            }
+            let userId;
+            if (ctx.update.message.from !== undefined) {
+                userId = ctx.update.message.from.id;
+            } else {
+                logger.error('delme: cannot find sender user. ctx:');
+                logger.error(JSON.parse(JSON.stringify(ctx)));
+                ctx.reply(`Ой, а кто это у нас такой скрытный здесь?🤡Даже не понять, кто-ты! (ошибка в определении юзера)`);
+                return;
+            }
+            const chatId = ctx.update.message.chat.id;
+
+            try {
+                await new Promise((resolve, reject) => {
+                    db.query(`DELETE FROM apartment_info WHERE chat_id = ? AND user_id = ?`, [chatId, userId], (error, results) => {
+                        if (error) {
+                            reject(error);
+                            return;
+                        }
+                        resolve(true);
+                    });
+                });
+            } catch (err) {
+                logger.error('aptslist: error on attempt to find existing record:');
+                logger.error(JSON.parse(JSON.stringify(err)));
+                ctx.reply(GENERAL_ERROR_MSG);
+                return;
+            }
+            ctx.reply(`Пока-пока🥲`);
+        });
+
         bot.help((ctx) => ctx.reply(`Запомни две команды. Всего лишь две.
         /setapt *номер квартиры* - расскажешь всем, в какой квартире живешь
         /aptcontacts *номер квартиры* - узнаешь, кто живет в этой квартире
 Понятное дело, что в одной квартире может проживать несколько человек. Но никто не может проживать в двух квартирах сразу. Поэтому, выбирай свою квартиру с умом🤓 
 Повторная команда /setapt *номер квартиры* всегда перезапишет предыдущий номер.
+Еще пара вспомогательных команд.
+        /aptslist - покажет все квартиры и тех, кто в них живет
+        /delme - удалит запись о тебе, если ты решил переехать или просто покинул чат
          `))
         bot.on('text', (ctx) => {
             ctx.reply(`Не знаю, что ты имеешь ввиду🤷`);
         });
     })
     .catch(error => logger.error(JSON.parse(JSON.stringify(error))))
+
+const renderContact = (contact) => {
+    if (contact.username !== null) {
+        return `[@${contact.username}](tg://user?id=${contact.user_id})`.replace(/_/g, "\\_");
+    } else {
+        return `[без юзернейма](tg://user?id=${contact.user_id})`
+    }
+}
+
+const renderContacts = (contacts) => contacts.map((contact) => renderContact(contact)).join(', ');
+
+const buildAptListMessages = (contacts) => {
+    return buildTgMessages(buildAptListLines(contacts));
+}
+
+const buildAptListLines = (contacts) => {
+    if (contacts.length === 0) {
+        return [`Здесь никто не живет🤷`]
+    }
+
+    const contactsByAptsNumber = {};
+    contacts.reduce((contactsByAptsNumber, contact) => {
+        if (contactsByAptsNumber[contact.apartment_number] === undefined) {
+            contactsByAptsNumber[contact.apartment_number] = [];
+        }
+        contactsByAptsNumber[contact.apartment_number].push(contact);
+        return contactsByAptsNumber;
+    }, contactsByAptsNumber)
+
+    const aptMessageLines = [];
+    for (let contact of contacts) {
+        if (contactsByAptsNumber[contact.apartment_number] !== undefined) {
+            aptMessageLines.push(`кв ${contact.apartment_number} : ${renderContacts(contactsByAptsNumber[contact.apartment_number])}`);
+            delete contactsByAptsNumber[contact.apartment_number];
+        }
+    }
+    return aptMessageLines;
+}
+
+const buildTgMessages = (lines) => {
+    const messages = [];
+    let currentMessageLength = 0;
+    let messageRowStartIndex = 0;
+    for (let i = 0; i < lines.length; i++) {
+        // +1 because of \n symbol
+        if (currentMessageLength + lines[i].length + 1 > TELEGRAM_MAX_MESSAGE_LENGTH) {
+            messages.push(lines.slice(messageRowStartIndex, i).join(`\n`));
+            messageRowStartIndex = i;
+            currentMessageLength = 0;
+        }
+        currentMessageLength += lines[i].length + 1;
+    }
+    messages.push(lines.slice(messageRowStartIndex).join(`\n`));
+    return messages;
+}
 
 const requestListener = function (req, res) {
     let body = '';
